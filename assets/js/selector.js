a21386ac0ae378289a97be0c023e0c5d86c20511
SaurFort <97303132+SaurFort@users.noreply.github.com>
2025-05-14 16:58:50 +0000

    Ajout css bouton

--- assets/js/selector.js
+++ assets/js/selector.js
@@ -3,18 +3,18 @@
 //apres cela on determine quel champs de validation de sexe doit etre desactivé.
 
 function sexes() {
-  let yalkSexe = document.getElementById("sexe_Yalk");
-  let kraithSexe = document.getElementById("sexe_Kraith");
-  let choix = document.getElementById("Espèce");
+  let yalkSexe = document.getElementById('sexe_Yalk')
+  let kraithSexe = document.getElementById('sexe_Kraith')
+  let choix = document.getElementById('Espèce')
 
-  if (choix.value == "1") {
-    kraithSexe.style.opacity = 1.0;
-    yalkSexe.style.opacity = 0.2;
+  if (choix.value == '1') {
+    kraithSexe.style.opacity = 1.0
+    yalkSexe.style.opacity = 0.2
   }
 
-  if (choix.value == "2") {
-    yalkSexe.style.opacity = 1.0;
-    kraithSexe.style.opacity = 0.2;
+  if (choix.value == '2') {
+    yalkSexe.style.opacity = 1.0
+    kraithSexe.style.opacity = 0.2
   }
 }
 
@@ -22,52 +22,52 @@ function sexes() {
 //de posséder un nom inadapté. Sois dans le lore de l'univers ou bien de
 //maniere générale avec des noms de figures historiques, des paradoxes ou juste pour la blague...
 const nomsInterdits = [
-  "xafully",
-  "teto",
-  "terrador",
-  "feryos",
-  "ikosso",
-  "ghurderal",
-  "orodar",
-  "arionyx",
-  "sotild",
-  "aly",
-  "the boy",
-  "boy",
-  "louna",
-  "alona",
-  "xoryus",
-  "yaran",
-  "tokati",
-  "hélix",
-  "kelos",
-  "hitler",
-  "staline",
-  "mussolini",
-  "franco",
-  "adolf hitler",
-  "crabe",
-  "crabes",
-  "kira",
-  "moi",
-  "toi",
-  "lui",
-  "bill",
-  "xx_Darkness92_xx",
-  "00110110 00111001"
-];
+  'xafully',
+  'teto',
+  'terrador',
+  'feryos',
+  'ikosso',
+  'ghurderal',
+  'orodar',
+  'arionyx',
+  'sotild',
+  'aly',
+  'the boy',
+  'boy',
+  'louna',
+  'alona',
+  'xoryus',
+  'yaran',
+  'tokati',
+  'hélix',
+  'kelos',
+  'hitler',
+  'staline',
+  'mussolini',
+  'franco',
+  'adolf hitler',
+  'crabe',
+  'crabes',
+  'kira',
+  'moi',
+  'toi',
+  'lui',
+  'bill',
+  'xx_Darkness92_xx',
+  '00110110 00111001',
+  'hakito',
+  'saurfort',
+]
 
 function verifierNom() {
-    let selection = document.getElementById("nomselectionner");
+  let selection = document.getElementById('nomselectionner')
 
-  const nomMinuscule = selection.value;
+  const nomMinuscule = selection.value
 
   if (nomsInterdits.includes(nomMinuscule.toLowerCase())) {
-    alert("Ce nom est invalide, veuillez en choisir un autre...");
+    alert('Ce nom est invalide, veuillez en choisir un autre...')
     disable.button_valider
   } else {
-    alert("Nom valide! Vous pouvez continuez!");
+    alert('Nom valide! Vous pouvez continuez!')
   }
-  
 }
-
