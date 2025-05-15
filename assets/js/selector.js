acebd002fd3cbd94abc2084e5b9029bce7f224a4
Hakitox1004 <nino@createlier.org>
2025-05-15 13:49:18 +0000

    selection de sexe

--- assets/js/selector.js
+++ assets/js/selector.js
@@ -3,16 +3,33 @@
 //apres cela on determine quel champs de validation de sexe doit etre desactivé.
 
 function sexes() {
+  let sexe0 = document.getElementById('0');
+  let sexe1 = document.getElementById('1');
+  let hommesexe = document.getElementById('homme')
+  let femmesexe = document.getElementById('femme')
+
   let yalkSexe = document.getElementById('sexe_Yalk')
   let kraithSexe = document.getElementById('sexe_Kraith')
   let choix = document.getElementById('Espèce')
 
+  
+
   if (choix.value == '1') {
+
+    sexe0.disabled = true
+    sexe1.disabled = true
+    hommesexe.disabled = false
+    femmesexe.disabled = false
     kraithSexe.style.opacity = 1.0
     yalkSexe.style.opacity = 0.2
   }
 
   if (choix.value == '2') {
+    
+    sexe0.disabled = false
+    sexe1.disabled = false
+    hommesexe.disabled = true
+    femmesexe.disabled = true
     yalkSexe.style.opacity = 1.0
     kraithSexe.style.opacity = 0.2
   }
@@ -57,17 +74,53 @@ const nomsInterdits = [
   '00110110 00111001',
   'hakito',
   'saurfort',
+  'desire',
+  'désiré',
+  'motherfucker',
+  'mother fucker',
+  'le mother fucker',
+  "pute",
+  "benladen",
+  'ben laden',
+  "mao",
+  "sex",
+  "sexe",
+  "pedo",
+  "nazi",
+  "terroriste",
+  "fuck",
+  "shit",
+  "pute",
+  "connard",
+  "batard",
+  "enculé",
+  "fdp",
+  "salot",
+  "raciste",
+  "coccinelle",
+  "frauline",
+  "kkk",
+  'negga',
+  'neggro',
+  'noir',
+  "bite",
+  
 ]
 
 function verifierNom() {
+  let testNom = document.getElementById('testNom')
   let selection = document.getElementById('nomselectionner')
 
   const nomMinuscule = selection.value
 
   if (nomsInterdits.includes(nomMinuscule.toLowerCase())) {
-    alert('Ce nom est invalide, veuillez en choisir un autre...')
-    disable.button_valider
+    boutton_valider.style.opacity = 0.2
+    button_valider.disabled = true
+    testNom.innerText = 'Votre pseudo est invalide'
+    //alert('Ce nom est invalide, veuillez en choisir un autre...')
   } else {
+    boutton_valider.style.opacity = 1.0
+    boutton_valider.disabled = false
     alert('Nom valide! Vous pouvez continuez!')
   }
 }
