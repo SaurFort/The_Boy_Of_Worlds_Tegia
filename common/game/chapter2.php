7a4aa9eba929b308d12a12a61e7eacdef57b043f
SaurFort <97303132+SaurFort@users.noreply.github.com>
2025-05-18 12:59:32 +0000

    Début du chapitre 2

--- common/game/chapter2.php
+++ common/game/chapter2.php
@@ -1,3 +1,8 @@
-<div class="hidden" id="chapter2">
-  <h1>Chapitre 2</h1>
+<div>
+  <div id="scene0">
+    <h1>Chapitre 2 - Ombres de la Discorde</h1>
+  </div>
+  <a id="nextScene" class="button hidden" onclick="nextScene()">Suivant</a>
 </div>
+
+<script src="../../assets/js/game/chapter2.js"></script>
